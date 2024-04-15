196a66e41a6bfd4f8cd60be32be50d3d4741171b
BenBet21 <benoit.beti@gmail.com>
2024-04-15 15:17:58 +0200

    update

--- exercices/prompt.c
+++ exercices/prompt.c
@@ -1,1 +1,46 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+    char cwd[1024];
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        printf("%s ~$ ", cwd);
+    }
+    else
+    {
+        perror("getcwd() error");
+        return 1;
+    }
+
+        char input[100];
+
+        while (1)
+    {
+        if (fgets(input, sizeof(input), stdin) != NULL)
+        {
+            printf("Command entered: %s", input);
+        }
+        else
+        {
+            printf("Error reading input.\n");
+            return 1;
+        }
+
+            printf("%s ~$ ", cwd);
+    }
+
+    (void)argc;
+    (void)argv;
+
+    return 0;
+}
