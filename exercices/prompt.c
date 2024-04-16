82ddce832a12ecb25cda226b39bc4c60295e95cf
BenBet21 <benoit.beti@gmail.com>
2024-04-16 10:50:34 +0200

    test prompt.c

--- exercices/prompt.c
+++ exercices/prompt.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
 /**
  * main - Entry point
@@ -10,37 +12,66 @@
  */
 int main(int argc, char **argv)
 {
-    char cwd[1024];
+	char *token;
+	char *cwd = getenv("PWD");
+	char *input = NULL;
+	size_t input_size = 0;
+	ssize_t read;
 
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-    {
-        printf("%s ~$ ", cwd);
-    }
-    else
-    {
-        perror("getcwd() error");
-        return 1;
-    }
+	if (cwd == NULL)
+	{
+		perror("getcwd() error");
+		return (1);
+	}
+		while (1)
+	{
+		printf("%s ~$ ", cwd);
+		read = getline(&input, &input_size, stdin);
+		if (read == -1)
+		{
+			printf("Error reading input.\n");
+			free(input);
+			return (1);
+		}
 
-        char input[100];
+		if (input[read - 1] == '\n')
+			input[read - 1] = '\0';
 
-        while (1)
-    {
-        if (fgets(input, sizeof(input), stdin) != NULL)
-        {
-            printf("Command entered: %s", input);
-        }
-        else
-        {
-            printf("Error reading input.\n");
-            return 1;
-        }
-
-            printf("%s ~$ ", cwd);
-    }
-
-    (void)argc;
-    (void)argv;
-
-    return 0;
+		if (strcmp(input, "exit") == 0)
+			break;
+			
+		token = strtok(input, " ");
+		while (token != NULL)
+			{
+				pid_t pid = fork();
+				if (pid == -1)
+				{
+					perror("fork error:");
+					return (1);
+				}
+				else if (pid == 0)
+				{
+					char *argv[] = {"/bin/bash", "-c",token , NULL};
+					if (execve(argv[0], argv, NULL) == -1)
+					{
+						perror("execve error:");
+						free(input);
+						return (1);
+					}
+				exit(0);
+			}
+			else
+			{
+				int status;
+				waitpid(pid, &status, 0);
+			}
+			token = strtok(NULL, " ");
+			}
+		
+		}
+		(void)argc;
+		(void)argv;
+		
+		free(input);
+		return (0);
 }
