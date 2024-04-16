83d11151680c86da3257a736581945d6761113ed
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-04-16 08:57:55 +0000

    update

--- shell.c
+++ shell.c
@@ -10,24 +10,30 @@
 int main() {
     char command[MAX_CMD_LEN];
     char *args[MAX_ARGS];
-    char *token;
+    int should_run = 1;
     pid_t pid;
-    int status;
-	int i = 0;
+    char *token;
+    int i;
 
-    while (1) {
-        printf("$ ");
+    while (should_run) {
+        /* Lire la commande de l'utilisateur */
+        printf("\033[36m@%s: $ ", getenv("PWD"));
         fflush(stdout);
 
         if (fgets(command, MAX_CMD_LEN, stdin) == NULL) {
-            printf("\n");
-            break;
+            break;  
         }
 
-        command[strcspn(command, "\n")] = '\0';
+        command[strcspn(command, "\n")] = '\0'; /* Supprimer le saut de ligne final */
 
-        /* Parse command and arguments */
-        
+        /* Quitter si la commande est "exit" */
+        if (strcmp(command, "exit") == 0) {
+            should_run = 0;
+            continue;
+        }
+
+        /* Tokenize la commande */
+        i = 0;
         token = strtok(command, " ");
         while (token != NULL && i < MAX_ARGS - 1) {
             args[i++] = token;
@@ -35,37 +41,24 @@ int main() {
         }
         args[i] = NULL;
 
-        /* Handle built-in commands */
-        if (strcmp(args[0], "exit") == 0) {
-            break;
-        } else if (strcmp(args[0], "env") == 0) {
-            extern char **environ;
-            char **env = environ;
-            while (*env) {
-                printf("%s\n", *env);
-                env++;
-            }
-            continue;
-        }
-
-        /* Fork and execute command */
+        /* Fork pour exécuter la commande */
         pid = fork();
 
         if (pid < 0) {
-            perror("fork");
-            exit(EXIT_FAILURE);
+            fprintf(stderr, "Erreur lors de la création du processus fils\n");
+            return 1;
         }
 
-        if (pid == 0) { /* Child process */
-            /* Search for command in PATH */
-            execvp(args[0], args);
-            /* If execvp fails, print error message */
-            fprintf(stderr, "%s: command not found\n", args[0]);
+        if (pid == 0) { /* Processus fils */
+            /* Exécuter la commande */
+            if (execvp(args[0], args) == -1) {
+                fprintf(stderr, "Erreur lors de l'exécution de la commande\n");
+            }
             exit(EXIT_FAILURE);
-        } else { /* Parent process */
-            waitpid(pid, &status, 0);
+        } else { /* Processus parent */
+            /* Attendre la fin du processus fils */
+            wait(NULL);
         }
     }
-
-    return 0;
+    return (0);
 }
