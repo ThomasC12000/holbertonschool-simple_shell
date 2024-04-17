eb2720e076a45ace58da596fd218fa612bc2cb95
BenBet21 <benoit.beti@gmail.com>
2024-04-17 15:40:26 +0200

    mis à jour execve

--- shell.c
+++ shell.c
@@ -14,142 +14,146 @@
  */
 int main(int argc, char **argv)
 {
-    char *token;
-    char *cwd = getenv("PWD");
-    char *input = NULL;
-    size_t input_size = 0;
-    ssize_t read;
+	char *token;
+	char *cwd = getenv("PWD");
+	char *input = NULL;
+	size_t input_size = 0;
+	ssize_t read;
 	pid_t pid;
 
-    if (cwd == NULL)
-    {
-        perror("getcwd() error");
-        return (1);
-    }
-
-    if (!isatty(fileno(stdin)))
-    {
-        read = getline(&input, &input_size, stdin);
-        if (read == -1)
-        {
-            printf("Error reading input.\n");
-            free(input);
-            return (1);
-        }
-
-        input[strcspn(input, "\n")] = '\0';
-
-        token = strtok(input, " ");
-        while (token != NULL)
-        {
-            if (access(token, X_OK) == -1)
-            {
-                printf("Command not found: %s\n", token);
-                break;
-            }
-			pid = fork();
-            if (pid == -1)
-            {
-                perror("fork error:");
-                return (1);
-            }
-            else if (pid == 0)
-            {
-                char **cmd_argv = malloc(sizeof(char *) * 4);
-                if (cmd_argv == NULL)
-                {
-                    perror("malloc error:");
-                    free(input);
-                    return (1);
-                }
-                cmd_argv[0] = "/bin/bash";
-                cmd_argv[1] = "-c";
-                cmd_argv[2] = token;
-                cmd_argv[3] = NULL;
-
-                if (execve(cmd_argv[0], cmd_argv, NULL) == -1)
-                {
-                    perror("execve error:");
-                    free(input);
-                    free(cmd_argv);
-                    return (1);
-                }
-                exit(0);
-            }
-            else
-            {
-                int status;
-                waitpid(pid, &status, 0);
-            }
-            token = strtok(NULL, " ");
-        }
-        free(input);
-        return 0;
-    }
-
-    while (1)
-    {
-        printf("%s ~$ ", cwd);
-    read = getline(&input, &input_size, stdin);
-    if (read == -1)
-    {
-        if (feof(stdin))
-        {
-            printf("\n");
-            break;
-        }
-        printf("Error reading input.\n");
-        free(input);
-        return (1);
+	if (cwd == NULL)
+	{
+		perror("getcwd() error");
+		return (1);
 	}
 
-        input[strcspn(input, "\n")] = '\0';
+	if (!isatty(fileno(stdin)))
+	{
+		read = getline(&input, &input_size, stdin);
+		if (read == -1)
+		{
+			printf("Error reading input.\n");
+			free(input);
+			return (1);
+		}
 
-        if (strcmp(input, "exit") == 0)
-            break;
+		input[strcspn(input, "\n")] = '\0';
 
-        token = strtok(input, " ");
-        while (token != NULL)
-        {
-            pid = fork();
-            if (pid == -1)
-            {
-                perror("fork error:");
-                return (1);
-            }
-            else if (pid == 0)
-            {
-                char **cmd_argv = malloc(sizeof(char *) * 4);
-                if (cmd_argv == NULL)
-                {
-                    perror("malloc error:");
-                    free(input);
-                    return (1);
-                }
-                cmd_argv[0] = "/bin/bash";
-                cmd_argv[1] = "-c";
-                cmd_argv[2] = token;
-                cmd_argv[3] = NULL;
+		token = strtok(input, " ");
+		while (token != NULL)
+		{
+			if (access(token, X_OK) == -1)
+			{
+				printf("Command not found: %s\n", token);
+				break;
+			}
+			pid = fork();
+			if (pid == -1)
+			{
+				perror("fork error:");
+				return (1);
+			}
+			else if (pid == 0)
+			{
+				char **cmd_argv = malloc(sizeof(char *) * 4);
 
-                if (execve(cmd_argv[0], cmd_argv, NULL) == -1)
-                {
-                    perror("execve error:");
-                    free(input);
-                    free(cmd_argv);
-                    return (1);
-                }
-                exit(0);
-            }
-            else
-            {
-                int status;
-                waitpid(pid, &status, 0);
-            }
-            token = strtok(NULL, " ");
-        }
-    }
+				if (cmd_argv == NULL)
+				{
+					perror("malloc error:");
+					free(input);
+					return (1);
+				}
+				cmd_argv[0] = "/bin/bash";
+				cmd_argv[1] = "-c";
+				cmd_argv[2] = token;
+				cmd_argv[3] = NULL;
+
+				if (execve(cmd_argv[0], cmd_argv, NULL) == -1)
+				{
+					perror("execve error:");
+					free(input);
+					free(cmd_argv);
+					return (1);
+				}
+				exit(0);
+			}
+			else
+			{
+				int status;
+				
+				waitpid(pid, &status, 0);
+			}
+			token = strtok(NULL, " ");
+		}
+		free(input);
+		return (0);
+	}
+
+	while (1)
+	{
+		printf("%s ~$ ", cwd);
+	read = getline(&input, &input_size, stdin);
+	if (read == -1)
+	{
+		if (feof(stdin))
+		{
+			printf("\n");
+			break;
+		}
+		printf("Error reading input.\n");
+		free(input);
+		return (1);
+	}
+
+		input[strcspn(input, "\n")] = '\0';
+
+		if (strcmp(input, "exit") == 0)
+			break;
+
+		token = strtok(input, " ");
+		while (token != NULL)
+		{
+			pid = fork();
+			if (pid == -1)
+			{
+				perror("fork error:");
+				return (1);
+			}
+			else if (pid == 0)
+			{
+				char **cmd_argv = malloc(sizeof(char *) * 4);
+
+				if (cmd_argv == NULL)
+				{
+					perror("malloc error:");
+					free(input);
+					return (1);
+				}
+				cmd_argv[0] = "/bin/bash";
+				cmd_argv[1] = "-c";
+				cmd_argv[2] = token;
+				cmd_argv[3] = NULL;
+
+				if (execve(cmd_argv[0], cmd_argv, NULL) == -1)
+				{
+					perror("execve error:");
+					free(input);
+					free(cmd_argv);
+					return (1);
+				}
+				exit(0);
+			}
+			else
+			{
+				int status;
+
+				waitpid(pid, &status, 0);
+			}
+			token = strtok(NULL, " ");
+		}
+	}
 	(void)argc;
 	(void)argv;
-    free(input);
-    return (0);
+	free(input);
+	return (0);
 }
