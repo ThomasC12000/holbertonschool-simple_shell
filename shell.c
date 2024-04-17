d43a028a88c56c413f8a45950f4b1d1f8b1dc6b5
Benoît BETI MARANGON <153932864+BenBet21@users.noreply.github.com>
2024-04-17 10:29:51 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,49 +1,144 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
-#define MAX_CMD_LEN 100
-#define MAX_ARGS 10
+#define MAX_INPUT_LENGTH 1024
 
 /**
- * main - Entry point for the shell program
- *
- * Return: Always 0
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ * Return: 0 on success
  */
-int main() 
+int main(int argc, char **argv)
 {
-    char command[MAX_CMD_LEN];
-    char *args[MAX_ARGS];
-    int should_run = 1;
     char *token;
-    int i;
+    char *cwd = getenv("PWD");
+    char *input = NULL;
+    size_t input_size = 0;
+    ssize_t read;
 
-    while (should_run) 
+    if (cwd == NULL)
     {
-        printf("\033[36m@%s: $ ", getenv("PWD"));
-        fflush(stdout);
+        perror("getcwd() error");
+        return (1);
+    }
 
-        if (fgets(command, MAX_CMD_LEN, stdin) == NULL) 
+    if (!isatty(fileno(stdin)))
+    {
+        read = getline(&input, &input_size, stdin);
+        if (read == -1)
         {
-            break;  
+            printf("Error reading input.\n");
+            free(input);
+            return (1);
         }
 
-        command[strcspn(command, "\n")] = '\0';
+        input[strcspn(input, "\n")] = '\0';
 
-        if (strcmp(command, "exit") == 0) 
+        token = strtok(input, " ");
+        while (token != NULL)
         {
-            should_run = 0;
-            continue;
-        }
+            pid_t pid = fork();
+            if (pid == -1)
+            {
+                perror("fork error:");
+                return (1);
+            }
+            else if (pid == 0)
+            {
+                char **cmd_argv = malloc(sizeof(char *) * 4);
+                if (cmd_argv == NULL)
+                {
+                    perror("malloc error:");
+                    free(input);
+                    return (1);
+                }
+                cmd_argv[0] = "/bin/bash";
+                cmd_argv[1] = "-c";
+                cmd_argv[2] = token;
+                cmd_argv[3] = NULL;
 
-        i = 0;
-        token = strtok(command, " ");
-        while (token != NULL && i < MAX_ARGS - 1) 
-        {
-            args[i++] = token;
+                if (execve(cmd_argv[0], cmd_argv, NULL) == -1)
+                {
+                    perror("execve error:");
+                    free(input);
+                    free(cmd_argv);
+                    return (1);
+                }
+                exit(0);
+            }
+            else
+            {
+                int status;
+                waitpid(pid, &status, 0);
+            }
             token = strtok(NULL, " ");
         }
-        args[i] = NULL;
-
-        execute_command(args);
+        free(input);
+        return 0;
     }
+
+    while (1)
+    {
+        printf("%s ~$ ", cwd);
+        read = getline(&input, &input_size, stdin);
+        if (read == -1)
+        {
+            printf("Error reading input.\n");
+            free(input);
+            return (1);
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0)
+            break;
+
+        token = strtok(input, " ");
+        while (token != NULL)
+        {
+            pid_t pid = fork();
+            if (pid == -1)
+            {
+                perror("fork error:");
+                return (1);
+            }
+            else if (pid == 0)
+            {
+                char **cmd_argv = malloc(sizeof(char *) * 4);
+                if (cmd_argv == NULL)
+                {
+                    perror("malloc error:");
+                    free(input);
+                    return (1);
+                }
+                cmd_argv[0] = "/bin/bash";
+                cmd_argv[1] = "-c";
+                cmd_argv[2] = token;
+                cmd_argv[3] = NULL;
+
+                if (execve(cmd_argv[0], cmd_argv, NULL) == -1)
+                {
+                    perror("execve error:");
+                    free(input);
+                    free(cmd_argv);
+                    return (1);
+                }
+                exit(0);
+            }
+            else
+            {
+                int status;
+                waitpid(pid, &status, 0);
+            }
+            token = strtok(NULL, " ");
+        }
+    }
+	(void)argc;
+	(void)argv;
+    free(input);
     return (0);
 }
