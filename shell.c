d3a83b535f3ad0f60d9eabf76db56b117f7c785c
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-04-17 11:36:29 +0000

    update shell thomas

--- shell.c
+++ shell.c
@@ -18,9 +18,11 @@ int main()
 
 	while (should_run) 
 	{
-		printf("\033[36m@%s: $ ", getenv("PWD"));
+		if (isatty(STDIN_FILENO))
+		{
+		printf("\033[32m@%s\033[0m:\033[36m%s\033[0m $ ", getenv("USER"), getenv("PWD"));
 		fflush(stdout);
-
+		}
 		if (fgets(command, MAX_CMD_LEN, stdin) == NULL) 
 		{
 			break;  
