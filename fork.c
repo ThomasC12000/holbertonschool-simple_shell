a2b20222647d9dbb7be04b5f7ab7f61cc58b7b2e
BenBet21 <benoit.beti@gmail.com>
2024-04-18 09:56:45 +0200

    mise à jour fork.c

--- fork.c
+++ fork.c
@@ -3,9 +3,9 @@
 /**
  * execute_command - Execute a command with its arguments
  * @args: Array of strings containing the command and its arguments
- *
  * Return: 0 on success, 1 on failure
  */
+
 int execute_command(char *args[])
 {
     pid_t pid;
@@ -15,7 +15,7 @@ int execute_command(char *args[])
     if (pid < 0) 
     {
         fprintf(stderr, "Erreur lors de la création du processus fils\n");
-        return 1;
+        return (1);
     }
     if (pid == 0) 
     {
@@ -30,5 +30,5 @@ int execute_command(char *args[])
     {
         wait(NULL);
     }
-    return 0;
+    return (0);
 }
